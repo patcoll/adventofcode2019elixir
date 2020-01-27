515d462b1ee51b0cc5f243901da5c890319f40a2
Patrick Collins <pat@burned.com>
2020-01-27 15:32:39 -0500

    Remove unused

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule Adventofcode2019elixir.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:flow, "~> 0.15.0"},
       # {:fun_with_flags, "~> 1.4.1"},
-      {:libgraph, "~> 0.7"},
+      # {:libgraph, "~> 0.7"},
       {:dialyxir, "~> 1.0.0-rc.3", only: [:dev, :test], runtime: false},
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
     ]
